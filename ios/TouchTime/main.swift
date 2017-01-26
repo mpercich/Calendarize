ab9d1f9a55380314f50816d49ccd07d0bf09a4b8
Michele Percich <mpercich@me.com>
2017-01-26 22:06:48 +0100

    implementation

---
+++ ios/TouchTime/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  touchtime
+//
+//  Created by Michele on 25/01/17.
+//  Copyright © 2017 Michele Percich. All rights reserved.
+//
+
+import Foundation
+
+//DJRKeyboardEvents.sendCommandC()
+//NSLog(DJRPasteboardProxy.selectedText())
+if let date = TouchTime.string(toDate: DJRPasteboardProxy.selectedText()) {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = DateFormatter.Style.medium
+    //print(dateFormatter.string(from: date))
+    //print(date.description)
+    NSLog(date.description)
+}
